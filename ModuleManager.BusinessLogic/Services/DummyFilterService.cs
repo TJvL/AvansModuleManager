6b692dc1da4033b20df47c3e608ae47563d0fa28
Nevernown <ca.boel@student.avans.nl>
2014-12-18 14:29:32 +0100

    Quickfix

--- ModuleManager.BusinessLogic/Services/DummyFilterService.cs
+++ ModuleManager.BusinessLogic/Services/DummyFilterService.cs
@@ -8,11 +8,19 @@ using System.Threading.Tasks;
 
 namespace ModuleManager.BusinessLogic.Services
 {
+    /// <summary>
+    /// Klasse om filters te vervangen zoalng er geen concrete implementatie (nodig) is.
+    /// </summary>
     public class DummyFilterService : IFilterService
     {
+        /// <summary>
+        /// passthrough
+        /// </summary>
+        /// <param name="toQuery">Een willekeuring queryable Pack</param>
+        /// <returns>Alle data van het queryablepack</returns>
         public IEnumerable Filter(IQueryablePack toQuery)
         {
-            return toQuery as IEnumerable;
+            return toQuery.GetData() as IEnumerable;
         }
     }
 }
