ec5fb4d3fae8c7d50d51d2c712d7439640f8b8bd
Paul Laros <pjg.laros@gmail.com>
2014-12-19 16:41:27 +0100

    Added datatables and configured

---
+++ ModuleManager.Web/Scripts/module-overview.js
@@ -0,0 +1,110 @@
+﻿$(function () {
+
+    /* Disable closing toggle-columns on click */
+    $("body").on("click", ".dropdown-menu", function (e) {
+        e.stopPropagation();
+    });
+        
+    init_datatable();
+    init_toggle_columns();
+    init_select_all();
+
+});
+
+function init_datatable() {
+    /* Datatables */
+    var table = $("#modules").dataTable({
+        dom:
+            "<'row'<'col-sm-6'l><'col-sm-6 toolbar text-right'>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-6'p><'col-sm-6'i>>",
+        processing: true,
+        serverSide: true,
+        language: {
+            url: "/Scripts/dataTables.dutch.js"
+        },
+        ajax: {
+            //url: '/api/Module/GetOverview',
+            //type: 'POST'
+            url: '/Scripts/temp_data.js',
+            type: 'GET'
+        },
+        columns: [
+            { "data": "Icon" },
+            { "data": "Naam" },
+            { "data": "CursusCode" },
+            { "data": "Blok" },
+            { "data": "Fase" },
+            { "data": "StudiePunten" },
+            { "data": "CursusCode" }
+        ],
+        aoColumnDefs: [
+            {
+                sClass: "text-center",
+                bSortable: false,
+                mRender: function (data, type, full) {
+                    return '<span class="fa fa-3x fa-' + data + '"></span>';
+                },
+                aTargets: [0]
+            },
+            {
+                mRender: function (data, type, full) {
+                    return data + "<br /><span class=\"text-muted\">" + full['Verantwoordelijke'] + "</span>";
+                },
+                aTargets: [1]
+            },
+            {
+                mRender: function (data, type, full) {
+                    return "Blok " + data;
+                },
+                aTargets: [3]
+            },
+            {
+                mRender: function (data, type, full) {
+                    return data + " EC";
+                },
+                aTargets: [5]
+            },
+            {
+                sClass: "text-center",
+                sTitle: "<input type=\"checkbox\" id=\"checkbox-select-all\"></input>",
+                mRender: function (data, type, full) {
+                    return "<input type=\"checkbox\" class=\"checkbox-module\" data-export=\"data\" />";
+                },
+                bSortable: false,
+                aTargets: [6]
+            }
+        ],
+        order: [[1, "asc"]],
+        initComplete: function () {
+
+            $(".toolbar").append($("#toggle-columns"));
+            $("#toggle-columns").show();
+        }
+    });
+}
+
+function init_toggle_columns() {
+    /* Toggle columns */
+    $(".toggle").on("click", function (e) {
+
+        // Get the column API object
+        var column = $("#modules").dataTable().api().column($(this).data("column"))
+
+        // Toggle the visibility
+        column.visible(!column.visible());
+    });
+}
+
+function init_select_all() {
+    /* Select all */
+    $("#modules").on("click", "#checkbox-select-all", function () {
+
+        if ($(this).is(":checked")) {
+            $('.checkbox-module').prop("checked", true);
+        } else {
+            $('.checkbox-module').prop("checked", false);
+        }
+
+    });
+}
