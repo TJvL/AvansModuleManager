f7a411dd8f4d608522437b5cef531a949acc37df
TJvL <tjleeuwe1@student.avans.nl>
2015-01-08 11:48:13 +0100

    klein werk

---
+++ ModuleManager.Web/ViewModels/PartialViewModel/ArgumentsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ModuleManager.Web.ViewModels.PartialViewModel
+{
+    public class ArgumentsViewModel
+    {
+        public int Draw { get; set; }
+        public ICollection<ColumnViewModel> Columns { get; set; }
+    }
+}
---
+++ ModuleManager.Web/ViewModels/PartialViewModel/ColumnViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ModuleManager.Web.ViewModels.PartialViewModel
+{
+    public class ColumnViewModel
+    {
+        public string Data { get; set; }
+        public string Name { get; set; }
+        public bool Searchable { get; set; }
+        public bool Orderable { get; set; }
+
+
+
+//columns[0][data]:Icon
+//columns[0][name]:
+//columns[0][searchable]:true
+//columns[0][orderable]:false
+//columns[0][search][value]:
+//columns[0][search][regex]:false
+    }
+}
