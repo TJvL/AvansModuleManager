73bdb81a1440cc3036ca224f4d104370284dc16e
thomas <tjleeuwe1@student.avans.nl>
2015-01-13 21:59:37 +0100

    Archiveer functie in back end

--- ModuleManager.BusinessLogic/Services/DummyModuleExporterService.cs
+++ ModuleManager.BusinessLogic/Services/DummyModuleExporterService.cs
@@ -4,11 +4,6 @@ using ModuleManager.BusinessLogic.Interfaces;
 using ModuleManager.BusinessLogic.Interfaces.Services;
 using ModuleManager.DomainDAL;
 using PdfSharp.Pdf;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ModuleManager.BusinessLogic.Services
 {
