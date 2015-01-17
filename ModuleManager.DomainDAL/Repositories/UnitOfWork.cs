7fc727baa8738c4bf4b25043b02c60a1b8a8dd2f
TJvL <tjleeuwe1@student.avans.nl>
2015-01-17 19:58:08 +0100

    fix

---
+++ ModuleManager.DomainDAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ModuleManager.DomainDAL.Interfaces;
+
+namespace ModuleManager.DomainDAL.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DomainContext _context;
+
+        public DomainContext Context
+        {
+            get { return _context ?? (_context = new DomainContext()); }
+        }
+
+        public void Dispose()
+        {
+            _context.SaveChanges();
+            _context.Dispose();
+        }
+    }
+}
