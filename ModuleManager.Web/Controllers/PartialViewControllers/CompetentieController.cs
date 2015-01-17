ff03fa4c708543bd752f1c86680b1c4527edb11e
Paul Laros <pjg.laros@gmail.com>
2015-01-17 19:30:29 +0100

    Curriculum Competentie CRUD part one

---
+++ ModuleManager.Web/Controllers/PartialViewControllers/CompetentieController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using ModuleManager.DomainDAL;
+using ModuleManager.DomainDAL.Interfaces;
+using ModuleManager.UserDAL;
+using ModuleManager.Web.ViewModels;
+using ModuleManager.Web.ViewModels.PartialViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ModuleManager.Web.Controllers.Api
+{
+    public class CompetentiesController : Controller
+    {
+        private readonly IGenericRepository<Competentie> _competentieRepository;
+        public CompetentiesController(IGenericRepository<Competentie> competentieRepository)
+        {
+            _competentieRepository = competentieRepository;
+        }
+
+        public ActionResult Create()
+        {
+            Competentie competentie = new Competentie();
+            return PartialView("~/Views/Admin/Curriculum/Competentie/_Add.cshtml", competentie);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Competentie entity)
+        {
+            var isSucces = _competentieRepository.Create(entity);
+            return Json(new { success = isSucces });
+        }
+
+        public ActionResult Edit(string code)
+        {
+            if (code == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Competentie competentie = _competentieRepository.GetOne(new object[] { code });
+
+            if (competentie == null)
+            {
+                return HttpNotFound();
+            }
+            
+            return PartialView("~/Views/Admin/Curriculum/Competentie/_Edit.cshtml", competentie);
+        }
+
+
+        
+    }
+}
