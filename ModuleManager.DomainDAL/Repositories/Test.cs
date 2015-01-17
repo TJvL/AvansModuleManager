7e8b48962029c40dcd8258f704abe60c3fd2ee36
Fahiem <fskarsod@student.avans.nl>
2015-01-18 00:29:02 +0100

    een laag diep navigation props

---
+++ ModuleManager.DomainDAL/Repositories/Test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleManager.DomainDAL.Repositories
+{
+    public static class Test
+    {
+        public static IQueryable<T> IncludeMultiple<T>(this IQueryable<T> query, params Expression<Func<T, object>>[] includes)
+            where T : class
+        {
+            if (includes != null)
+            {
+                query = includes.Aggregate(query,
+                          (current, include) => current.Include(include));
+            }
+
+            return query;
+        }
+
+        public static IQueryable<T> IncludeMultiple<T>(this IQueryable<T> query, string[] includes)
+            where T : class
+        {
+            if (includes != null)
+            {
+                query = includes.Aggregate(query,
+                          (current, include) => current.Include(include));
+            }
+
+            return query;
+        }
+
+        public static string[] GetNavigationPropertiesNameOfClass<T>()
+            where T : class
+        {
+            List<string> propertyList = new List<string>();
+            propertyList.AddRange(typeof(T).GetProperties()
+                .Where(p => (typeof(IEnumerable).IsAssignableFrom(p.PropertyType) && p.PropertyType != typeof(string)) || p.PropertyType.Namespace == typeof(T).Namespace)
+                .Select(prop => prop.Name));
+            return propertyList.ToArray();
+        }
+    }
+}
--- ModuleManager.Web/Controllers/HomeController.cs
+++ ModuleManager.Web/Controllers/HomeController.cs
@@ -1,16 +1,36 @@
 ﻿using System.Web.Mvc;
+using ModuleManager.DomainDAL;
+using ModuleManager.DomainDAL.Repositories;
 
-namespace ModuleManager.Web.Controllers {
+namespace ModuleManager.Web.Controllers
+{
 
-    public class HomeController : Controller {
+    public class HomeController : Controller
+    {
 
         [HttpGet]
-        public ActionResult Index() {
+        public ActionResult Index()
+        {
             return View();
         }
 
         [HttpGet, Route("Home/Login")]
-        public ActionResult Login() {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        public ActionResult Yolo()
+        {
+            var repo = new GenericRepo();
+
+            var test = repo.GetAll<Fase>();
+
+            var testnormaal = repo.GetAllNormaal();
+            var testinclude = repo.GetAllInclude();
+            var testincludeMulti = repo.GetAllIncludeMultiple();
+
             return View();
         }
 
