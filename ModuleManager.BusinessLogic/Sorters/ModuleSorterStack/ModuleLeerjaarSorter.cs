744650b80f303cc91d992dfd4e57ac0a5af4db11
Nevernown <ca.boel@student.avans.nl>
2015-01-17 09:30:02 +0100

    Sorter Update (+ missing files fix)

---
+++ ModuleManager.BusinessLogic/Sorters/ModuleSorterStack/ModuleLeerjaarSorter.cs
@@ -0,0 +1,33 @@
+﻿using ModuleManager.BusinessLogic.Data;
+using ModuleManager.BusinessLogic.Interfaces.Sorters;
+using ModuleManager.DomainDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleManager.BusinessLogic.Sorters.ModuleSorterStack
+{
+    public class ModuleLeerjaarSorter : ModuleBaseSorter
+    {
+        public ModuleLeerjaarSorter(ISorter<Module> parent) : base(parent) { }
+
+        public override IQueryable<Module> Sort(IQueryable<Module> toSort, Arguments args) 
+        {
+            if (args.SortBy.Equals("Schooljaar")) 
+            {
+                if (args.SortDesc) 
+                {
+                    toSort = toSort.OrderByDescending(element => element.Schooljaar);
+                }
+                else 
+                {
+                    toSort = toSort.OrderBy(element => element.Schooljaar); 
+                }
+            }
+
+            return base.Sort(toSort, args);
+        }
+    }
+}
