773e30a716499604aa341b2b221a5038db31c52e
Nevernown <ca.boel@student.avans.nl>
2015-01-19 15:57:22 +0100

    Base for Lessen tabel exporteren. INCOMPLETE Needs a Service.

---
+++ ModuleManager.BusinessLogic/Data/LessenTabelExportArguments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleManager.BusinessLogic.Data
+{
+    public class LessenTabelExportArguments : AbstractExportArguments
+    {
+
+        public bool ExportNaam { get; set; }
+    }
+}
---
+++ ModuleManager.BusinessLogic/Exporters/LessenTabelPassiveExporter.cs
@@ -0,0 +1,28 @@
+﻿using MigraDoc.DocumentObjectModel;
+using ModuleManager.BusinessLogic.Interfaces.Exporters;
+using ModuleManager.DomainDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleManager.BusinessLogic.Exporters
+{
+    /// <summary>
+    /// The base exporter, does nothing except return.
+    /// </summary>
+    public class LessenTabelPassiveExporter : IExporter<FaseType>
+    {
+        /// <summary>
+        /// Returns an unmodified PDF Document.
+        /// </summary>
+        /// <param name="toExport">The raw data from which to build the PDF</param>
+        /// <param name="pdf">The document to build on</param>
+        /// <returns>Unmodified input "pdf"</returns>
+        public Section Export(FaseType toExport, Section sect)
+        {
+            return sect;
+        }
+    }
+}
