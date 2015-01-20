83a3a93fd0d5fe7d337191748ec7a80f2430d757
Fahiem <fskarsod@student.avans.nl>
2015-01-20 22:14:24 +0100

    error message in nederlands

--- ModuleManager.DomainDAL/Utility/DomainConstants.cs
+++ ModuleManager.DomainDAL/Utility/DomainConstants.cs
@@ -3,11 +3,11 @@
     public class DomainConstants
     {
         public const string DbErrorStandard = "Kan actie niet voltooien: Probeer het later nog eens.";
-        public const string DbErrorPkInvalid = "Kan actie niet voltooien: De opgegeven primaire sleutels zijn niet correct.";
-        public const string DbErrorFkConstraint = "Kan actie niet voltooien: De entiteit is een vreemde sleutel in een andere entiteit.";
-        public const string DbErrorPkDuplicate = "Kan actie niet voltooien: Deze primaire sleutel bestaat al in de tabel.";
-        public const string DbErrorNoSelection = "Kan actie niet voltooien: Er is niks geselecteerd om in te voeren.";
-        public const string DbErrorPkChanged = "Kan actie niet voltooien: Kan de primaire sleutel niet aanpassen van een entiteit.";
+        public const string DbErrorPkInvalid = "Kan actie niet voltooien: Een opgegeven veld is niet correct ingevuld.";
+        public const string DbErrorFkConstraint = "Kan actie niet voltooien: Een veranderd veld mag niet veranderd worden.";
+        public const string DbErrorPkDuplicate = "Kan actie niet voltooien: De ingevoerde Naam en/of Code bestaat al.";
+        public const string DbErrorNoSelection = "Kan actie niet voltooien: Een opgegeven veld is niet correct ingevuld.";
+        public const string DbErrorPkChanged = "Kan actie niet voltooien: Een van de velden mag niet aangepast worden.";
 
         public const string ExportErrorStandard = "Kan actie niet voltooien.";
         public const string ExportErrorAccessDenied = "Kan actie niet voltooien: Bestandssysteem weigerd toegang tot bestand of bestandslocatie.";
